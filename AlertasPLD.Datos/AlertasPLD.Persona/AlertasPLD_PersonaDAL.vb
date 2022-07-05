11cd9e4dccde1200456c0d46844b86640e7507a1
JONNYJSC <jjonath21@gmail.com>
2022-07-04 23:37:56 -0600

    Agregado metodo para mostrar mediante ListView en la capa Datos

--- AlertasPLD.Datos/AlertasPLD.Persona/AlertasPLD_PersonaDAL.vb
+++ AlertasPLD.Datos/AlertasPLD.Persona/AlertasPLD_PersonaDAL.vb
@@ -19,4 +19,22 @@ Public Module AlertasPLD_PersonaDAL
         End Using
     End Function
 
+    Public Function mostrarPersonasListViiew() As DataTable
+        Using connection = Conexion.GetConexion()
+            connection.Open()
+            Using comando = New SqlCommand()
+                comando.Connection = connection
+                comando.CommandText = "SP_SEL_Persona"
+                comando.CommandType = CommandType.StoredProcedure
+                Dim SqlAdapter = New SqlDataAdapter(comando)
+                Dim tabla = New DataTable("Persona")
+                SqlAdapter.Fill(tabla)
+                comando.Dispose()
+                SqlAdapter.Dispose()
+                comando.Parameters.Clear()
+                Return tabla
+            End Using
+        End Using
+    End Function
+
 End Module
