8eac8806a59305d2e0ccb5af05e89c3bc7fd491a
JONNYJSC <jjonath21@gmail.com>
2022-07-04 23:40:17 -0600

    Agregando listarPersonasListiView en la capa de Logica

--- AlertasPLD.Logica/AlertasPLD.Persona/AlertasPLD_PersonaBIL.vb
+++ AlertasPLD.Logica/AlertasPLD.Persona/AlertasPLD_PersonaBIL.vb
@@ -4,4 +4,8 @@ Public Module AlertasPLD_PersonaBIL
     Public Function listarPersonas() As DataTable
         Return AlertasPLD_PersonaDAL.mostrarPersonas
     End Function
+
+    Public Function listarPersonasListView() As DataTable
+        Return AlertasPLD_PersonaDAL.mostrarPersonasListViiew
+    End Function
 End Module
